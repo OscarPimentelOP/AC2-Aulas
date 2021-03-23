ab854fd2d65537a75d5f8ca91fadf0d2cec8242d
Oscar Pimentel <oscarpimentel@ua.pt>
2021-03-23 12:02:23 +0000

    Added ex2, delay function

---
+++ Guiao2/delay.s
@@ -0,0 +1,33 @@
+ #Mapeamento de variáveis:
+ #ms -> $t0
+
+.equ READ_CORE_TIMER, 11
+.equ RESET_CORE_TIMER, 12
+.equ K, 10000
+
+.data
+.text
+
+.globl main
+
+main:
+    move $v0, $a0
+
+for:
+    ble $t0, 0, endFor # for(; ms > 0; ms--)
+
+    li $v0, RESET_CORE_TIMER # resetCoreTimer();
+    syscall
+
+while:  # while(readCoreTimer() < K);
+    li $v0, READ_CORE_TIMER # readCoreTimer()
+    blt $v0, K, while
+
+    subu $t0, $t0, 1 # ms--
+    j for 
+
+endFor: 
+    jr $ra    
+
+
+
