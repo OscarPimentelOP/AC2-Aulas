55889cd838e201646886586f68218560d3bbdd2d
Oscar Pimentel <oscarpimentel@ua.pt>
2021-05-03 14:18:20 +0100

    Update ex2.c

--- TesteModelo/ex2.c
+++ TesteModelo/ex2.c
@@ -15,6 +15,7 @@ int main(void){
         if((input == '0') || (input == '1') || (input == '2') || (input == '3')){
             delay(1000); //update display in 100Hz
             send2displays(toBcd(input));
+            LATB = (LATB & 0x00FF); //clean RB8-15
         }
         else{
             send2displays(15);
