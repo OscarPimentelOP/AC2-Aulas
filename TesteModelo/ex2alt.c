63a297ed1bcd7467fb1973ffc055ef1fbcb92087
Oscar Pimentel <oscarpimentel@ua.pt>
2021-05-03 16:02:10 +0100

    [teste modelo ex 2] another one

--- TesteModelo/ex2alt.c
+++ TesteModelo/ex2alt.c
@@ -17,20 +17,19 @@ int main(void){
             //send2displays(toBcd(input));
         //}
         if(input == '0'){
-            send2displays(0x00);
+            send2displays(0x3F00, 0x3F00);
         }
         else if(input == '1'){
-            send2displays(0x01);
+            send2displays(0x3F00, 0x0600);
         }
         else if(input == '2'){
-            send2displays(0x02);
+            send2displays(0x3F00, 0x5B00);
         }
         else if(input == '3'){
-            send2displays(0x03);
+            send2displays(0x3F00, 0x4F00);
         }
         else{
-            send2displays(0xFF);
-            LATB = (LATB & 0x00FF); //clean RB8-15
+            send2displays(0x7100, 0x7100);
         }
         delay(10);  //10 ms -> f = 100 Hz
 
@@ -45,7 +44,7 @@ void delay(int ms){
     }
 }  
 
-void send2displays(unsigned char value) {
+void send2displays(unsigned char value1, unsigned char value2) {
 
     static const char display7Scodes[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x67, 0x77, 0x7C, 0x39, 0x5E, 0x79, 0x71};
     static char displayFlag = 0;
@@ -63,14 +62,14 @@ void send2displays(unsigned char value) {
 
     //send digit_low to display low
     if(displayFlag == 0){
-        LATB = (LATB & 0x00FF) | (display7Scodes[digit_low] << 8);
+        LATB = (LATB & 0x00FF) | value1;
         LATDbits.LATD5 = 1;
         LATDbits.LATD6 = 0;
     }
 
     //send digit_high to display high
     else{
-        LATB = (LATB & 0x00FF) | (display7Scodes[digit_high] << 8);
+        LATB = (LATB & 0x00FF) | value2;
         LATDbits.LATD5 = 0;
         LATDbits.LATD6 = 1;
     }
